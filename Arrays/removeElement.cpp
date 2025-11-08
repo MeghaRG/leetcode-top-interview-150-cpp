5fd8526d92714a28aa325945e66a8295687fc15a
MeghaRG <54666674+MeghaRG@users.noreply.github.com>
2025-11-08 11:34:18 +0530

    added remove element problem solution

---
+++ Arrays/removeElement.cpp
@@ -0,0 +1,4 @@
+//https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+//88. Merge Sorted Array
+//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
